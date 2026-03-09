b25b576f15f6b3d8600db81fc5042af6d5cb3cbf
Théo Schmitt <schmitt1690@gmail.com>
2026-03-09 16:17:18 +0100

    Remove contact form
    Comment out the Contact component in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import {
   Timeline,
   Expertise,
   Project,
-  Contact,
+  // Contact,
   Navigation,
   Footer,
 } from "./components";
@@ -34,7 +34,6 @@ function App() {
             <Expertise/>
             <Timeline/>
             <Project/>
-            {/* <Contact/> */}
         </FadeIn>
         <Footer />
     </div>
